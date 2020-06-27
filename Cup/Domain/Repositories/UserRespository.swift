8ebe5e9284512063c3c3c7ed22f45adc06c4b4e8
Henrique Jardim <henrique@MacBook-Pro-de-Henrique.local>
2020-06-27 16:53:59 -0300

    adiciondo arquitetura inicial

---
+++ Cup/Domain/Repositories/UserRespository.swift
@@ -0,0 +1,13 @@
+//
+//  UserRespository.swift
+//  Cup
+//
+//  Created by Henrique Jardim on 27/06/20.
+//  Copyright © 2020 Jardim. All rights reserved.
+//
+
+import Foundation
+
+protocol UserRespository {
+    func saveNickname(nickname : String)
+}
